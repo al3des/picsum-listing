549e2dee9ce7e6f24ff5f631914d9727da928840
al3des <alejandrodesalvo@gmail.com>
2020-12-12 10:03:44 +0100

    added correct fetching data method
    added correct fetching data method

--- pages/products/[productId].js
+++ pages/products/[productId].js
@@ -15,29 +15,12 @@ export default function productId({ image }) {
   );
 }
 
-// This function gets called at build time
-export async function getStaticPaths() {
-  // Call an external API endpoint to get posts
-  const res = await fetch("https://picsum.photos/v2/list");
-  const images = await res.json();
-
-  // Get the paths we want to pre-render based on posts
-  const paths = images.map((image) => ({
-    params: { productId: image.id },
-  }));
-
-  // We'll pre-render only these paths at build time.
-  // { fallback: false } means other routes should 404.
-  return { paths, fallback: false };
-}
-
-// This also gets called at build time
-export async function getStaticProps({ params }) {
-  // params contains the post `id`.
-  // If the route is like /posts/1, then params.id is 1
-  const res = await fetch(`https://picsum.photos/id/${params.productId}/info`);
-  const image = await res.json();
-
-  // Pass post data to the page via props
-  return { props: { image } };
+export async function getServerSideProps(context) {
+  console.log(context.query);
+  let { productId } = context.params;
+  let res = await fetch(`https://picsum.photos/id/${productId}/info`);
+  let image = await res.json();
+  return {
+    props: { image }, // will be passed to the page component as props
+  };
 }
